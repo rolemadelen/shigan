96be2ea2367e8d98b6b58ec2912e2873c9658c63
Madelen Yoo <rolemadelen@tuta.io>
2024-04-06 11:08:58 -0400

    command line options with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about = "Command line Pomodoro Timer", long_about = None)]
+struct Args {
+    #[arg(short, long, help="add <TASK> to the tracker", value_name = "TASK" )]
+    add: String,
+
+    #[arg(short, long, help="delete <TASK> from the tracker", value_name = "TASK" )]
+    delete: String,
+
+    #[arg(long, help="start the tracker for <TASK>", value_name = "TASK" )]
+    start: String,
+
+    #[arg(long, help="stops currently running tracker")]
+    stop: bool,
+
+    #[arg(short, long, help="list accumulated time for <TASK>", default_value = "all", value_name = "TASK")]
+    log: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+    
+    println!("hello");
 }
